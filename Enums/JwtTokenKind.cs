cb5ff973b76481294dbfda58578257fdb87fbb95
Kinoshita Shimizu <ks@kinoshitaproductions.com>
2025-09-05 18:55:11 -0600

    Add needed facilities for unit testing and improving auth workflow

--- Enums/JwtTokenKind.cs
+++ Enums/JwtTokenKind.cs
@@ -1,8 +1,10 @@
 namespace KinoshitaProductions.Common.Enums;
 
+[Flags]
 public enum JwtTokenKind
 {
-    Elevated,
-    App,
-    Session,
+    NotSpecified = 0,
+    Elevated = 1,
+    App = 2,
+    Session = 4,
 }
